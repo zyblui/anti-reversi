077728d1782c965d72fe54be79137cb99db03045
zyblui <zyblui@outlook.com>
2025-02-25 16:58:19 +0800

    Add files via upload

--- w.js
+++ w.js
@@ -1,3 +1,21 @@
 onmessage=function(e){
     if(e.data=="computerPlay") this.postMessage(cpu());
 }
+function cpu() {
+    let result = /*initSearchAlpha(board, searchDepth, playerColor)*/initSearchSort(board, searchDepth, playerColor);
+    console.log(result)
+    let biggestValue = Math.max(...result.map((x) => x.evaluation))
+    for (let r of result) {
+        if (r.evaluation == biggestValue) {
+            for (let i = 0; i <= 7; i++) {
+                for (let j = 0; j <= 7; j++) {
+                    if (board[i][j] == 0 && r.board[i][j] != 0) {
+                        /*pd(LETTERS[j] + (i + 1));
+                        return;*/
+                        return LETTERS[j] + (i + 1);
+                    }
+                }
+            }
+        }
+    }
+}
