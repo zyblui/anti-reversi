b620ea1d50d8b6cca1696fb683a77cd0794393ae
zyblui <zyblui@outlook.com>
2025-02-27 12:51:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -172,10 +172,7 @@ function render() {
         span2.innerText=previousMoves[i][1];
         let spanContainer=document.createElement("span");
         spanContainer.appendChild(span1);
-        let text=document.createTextNode(" ");
-        spanContainer.appendChild(text);
         spanContainer.appendChild(span2);
-        spanContainer.appendChild(text.cloneNode(false));
         document.getElementById("notation").appendChild(spanContainer);
         span1.addEventListener("click", function () {
             navigate(i, 0);
