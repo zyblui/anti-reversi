f070f13b336466d609846613fbdb02ce5c891f63
zyblui <zyblui@outlook.com>
2025-03-15 15:11:33 +0800

    Add files via upload

--- w.js
+++ w.js
@@ -67,7 +67,6 @@ function placeDisc(currentBoard, x, y, color) {
         let dirFlip = directionalFlip(tempBoard, x, y, i, color);
         if (dirFlip.flip) {
             isValidMove = true;
-            tempBoard = dirFlip.board;
         }
     }
     if (isValidMove) {
@@ -80,21 +79,19 @@ function placeDisc(currentBoard, x, y, color) {
     return { isValid: false }
 }
 function directionalFlip(currentBoard, x, y, direction, color) {
-    let tempBoard = JSON.parse(JSON.stringify(currentBoard))
     let flipCounter = 0;
     do {
         flipCounter++;
         if (!(x + direction[0] * flipCounter >= 0 && x + direction[0] * flipCounter <= 7 && y + direction[1] * flipCounter >= 0 && y + direction[1] *
-            flipCounter <= 7) || !tempBoard[x + direction[0] * flipCounter][y + direction[1] * flipCounter]) return { flip: false };
-    } while (tempBoard[x + direction[0] * flipCounter][y + direction[1] * flipCounter] == -color);
+            flipCounter <= 7) || !currentBoard[x + direction[0] * flipCounter][y + direction[1] * flipCounter]) return { flip: false };
+    } while (currentBoard[x + direction[0] * flipCounter][y + direction[1] * flipCounter] == -color);
     flipCounter--;
     if (!flipCounter) return { flip: false };
     for (let i = 1; i <= flipCounter; i++) {
-        tempBoard[x + direction[0] * i][y + direction[1] * i] = color;
+        currentBoard[x + direction[0] * i][y + direction[1] * i] = color;
     }
     return {
-        flip: true,
-        board: tempBoard
+        flip: true
     };
 }
 function getValidMoves(currentBoard, color) {
